2ad307e5da621d270aa00be642d871fe5795edc2
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-11-02 16:27:56 -0300

    alta pedido simple

--- Aplicacion/main.py
+++ Aplicacion/main.py
@@ -5,8 +5,7 @@ from peewee import *
 #import psycopg2
 from Entities.cliente import Cliente
 from Entities.cuenta import Cuenta
-from Entities.cuenta import Cuenta
-from Entities.tarjeta import Tarjeta
+from Entities.Tarjeta import Tarjeta
 from Entities.cobro import Cobro
 from Entities.pedidoCompuesto import PedidoCompuesto
 from Entities.pedidoSimple import PedidoSimple
@@ -23,6 +22,8 @@ def mostrarMenu():
     print("1) Alta cliente")
     print("2) Baja cliente")
     print("3) Modificar cliente")
+    print("5) Ingresar pedido Simple ")
+    print("6) Agregar Producto")
     num =input("NUMERO DEL MENU: ")
     return num
 
@@ -68,7 +69,7 @@ def modificarCliente(dni, nuevoNombre, nuevoApellido, nuevoCelular, nuevoMail, n
     #query = Cliente.update({Cliente.celular:nuevoCelular, Cliente.mail:nuevoMail, Cliente.nombre:nuevoNombre, Cliente.apellido:nuevoApellido, Cliente.departamento:nuevoDepartamento,Cliente.apartamento:nuevoApartamento,Cliente.calle:nuevaCalle,Cliente.codigo_postal:nuevoCodigoPostal,Cliente.localidad:nuevaLocalidad,Cliente.numero_puerta:nuevaPuerta}).where(Cliente.dni==dni)
     query = Cliente.update({Cliente.celular:nuevoCelular}).where(Cliente.dni==dni)
 
-def ingresarStock(codigo_producto, precio, nombre = None, stock = None, qr = "1"):
+def ingresarStock(codigo_producto, stock ,precio = None, nombre = None, qr = "1"):
     if (Producto.get(Producto.codigo_producto == codigo_producto) == None):
         neuvoProducto = Producto.create(codigo_producto = codigo_producto, precio = precio, nombre = nombre, stock = stock, qr = qr)
     else:
@@ -77,20 +78,23 @@ def ingresarStock(codigo_producto, precio, nombre = None, stock = None, qr = "1"
 def altaPedidoProducto(cantidad, codigo_producto, id_pedido_simple):
     nuevoPedidoProducto = productosPedido(cantidad = cantidad, codigo_producto = codigo_producto, id_pedido_simple = id_pedido_simple)
 
-
-
-def altaPedidoSimple(estado, fecha, canalDeCompra, nro_pedido_compuesto,dnicliente):
+def altaPedidoSimple(estado, fecha, canalDeCompra, dnicliente, nro_pedido_compuesto = None ):
     pedidoSimple = PedidoSimple.create(precio_total = 0, estado = estado, fecha = fecha, canal_de_compra = canalDeCompra, nro_pedido_compuesto = nro_pedido_compuesto, dni_cliente = dnicliente)
+
+    return pedidoSimple
+
+def calcularPrecioTotal(pedidoSimple):
     listaProductos = productosPedido.get(productosPedido.id_pedido_simple == pedidoSimple.id)
 
     for producto in listaProductos:
-        if( (Producto.get(Producto.codigo_producto == producto.codigo_producto)).stock > producto.cantidad):
+        if ((Producto.get(Producto.codigo_producto == producto.codigo_producto)).stock > producto.cantidad):
             precioTotal = precioTotal + producto.precio
-            (Producto.get(Producto.codigo_producto == producto.codigo_producto)).stock = (Producto.get(Producto.codigo_producto == producto.codigo_producto)).stock - producto.cantidad
+            (Producto.get(Producto.codigo_producto == producto.codigo_producto)).stock = (Producto.get(
+                Producto.codigo_producto == producto.codigo_producto)).stock - producto.cantidad
         else:
             return "no hay suficiente stock"
 
-    pedidoSimple.precio = precioTotal
+    return precioTotal
 
 #creo cliente -> creo cuenta creo tarjeta
 #
@@ -141,3 +145,62 @@ if __name__ == "__main__":
             numero_puerta1 = input("NUMERO PUERTA: ")
             nombre_usuario1 = input("NOMBRE DE USUARIO: ")
             modificarCliente(dni1, nombre1, apellido1, celular1, mail1, departamento1, calle1, codigo_postal1, apartamento1, localidad1, numero_puerta1, nombre_usuario1)
+
+        elif(menu == "5"):
+            print("Haga su pedido")
+            dni_cliente = input("Ingrese su DNI")
+            sigo = input("Precione 1 si quiere agreagr un producto o  0 si ya termino su pedido")
+
+            pedido_simple = altaPedidoSimple("por confirmar", datetime.now(), "visa", dni_cliente)
+
+            cantidad_productos = 0
+
+            while(sigo == 1 & cantidad_productos < 21):
+                codigo_producto = input("Numero del producto: ")
+                cantidad_del_producto = input("Cantidad del poducto que quiere llevar")
+                altaPedidoProducto( int(cantidad_del_producto), int(codigo_producto), pedido_simple )
+
+                pedido_simple.precio_total = calcularPrecioTotal(pedido_simple)
+
+                cantidad_productos = cantidad_productos + int(cantidad_del_producto)
+
+                sigo = input("Precione 1 si quiere agreagr un producto o  0 si ya termino su pedido")
+
+            if(cantidad_productos > 20):
+                print("excedio la cantidad de productos posibles")
+                borrar_pedido_simple = PedidoSimple.delete().where(PedidoSimple.id == pedido_simple.id)
+                borrar_pedido_simple.execute()
+                break
+
+            cuenta = Cuenta.get(Cuenta.dni_cliente == dni_cliente)
+
+            cobro = Cobro.create(id_predido = pedido_simple.id, numero_cuenta = cuenta.numero_cuenta, aprobado = False, numero_aprovacion = None)
+            
+            tarjeta = Tarjeta.get(Tarjeta.numero_cuenta == cuenta.numero_cuenta)
+            
+            if(tarjeta != None):  # hay q ver que devuelve un get que no encontro nada 
+                cobro.aprobado = True
+                print("Su pedido fue recivido con excito")
+        
+        elif(menu == 6):
+            print( "Agruegue el producto que quiera agregar:")
+            
+            numero_producto = input ("Ingrese numero de producto: ")
+            stock = input("Ingrese la cantidad de stock: ")
+            
+            if(Producto.get(Producto.codigo_producto == numero_producto) != None):
+                ingresarStock(int(numero_producto), int(stock))
+
+            else:
+                precio = input("Ingrese precio del producto: ")
+                nombre = input("Ingrese nombre del producto: ")
+
+                ingresarStock(int(numero_producto), int(stock), int(precio), nombre)
+
+
+            
+            
+            
+            
+            
+            
